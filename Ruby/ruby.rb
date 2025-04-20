bf7223e0a300b71ce14bdce2e5c284b71dbdabab
itsfirozahemad786 <thefirozshah777@gmail.com>
2025-04-20 14:00:41 +0530

    Update ruby.rb
    Additon of code by Ankur & Firoz

--- Ruby/ruby.rb
+++ Ruby/ruby.rb
@@ -3,3 +3,8 @@ this is ruby file and we are going to develop payment gateway using ruby languag
 
 
   upi gw has been added with logic & our base upi accepting applciation is PhonePe
+
+
+This changes were done by Scripter - Ankur
+
+2nd change regarding Google pay UPI addition was done by Firoz
